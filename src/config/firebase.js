f4d0d1cc78d91422eabcef0d598196c7ab9c9a8d
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2025-01-19 08:23:53 +0530

    s

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDpOk4QbFFp2X5AJZD6GhRWDnorSnfb1g4",
+  authDomain: "profilemanager-8d397.firebaseapp.com",
+  projectId: "profilemanager-8d397",
+  storageBucket: "profilemanager-8d397.firebasestorage.app",
+  messagingSenderId: "159723671464",
+  appId: "1:159723671464:web:f7a2ef0de19e126dd6d79d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
